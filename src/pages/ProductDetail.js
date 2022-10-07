3ae689bceffadeb7d86f64c289e7fe61575862d7
Suxrob9966 <rabbimovich9966@gmail.com>
2022-10-07 12:17:31 -0500

    extracting route params

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,7 +1,14 @@
+import { useParams } from "react-router-dom";
+
 const ProductDetail = () => {
+  const params = useParams(); //params is an object that consists of key value pair
+
+  console.log(params.productId);
+
   return (
     <section>
       <h1>Product Detail</h1>
+      <p>{params.productId}</p>
     </section>
   );
 };
